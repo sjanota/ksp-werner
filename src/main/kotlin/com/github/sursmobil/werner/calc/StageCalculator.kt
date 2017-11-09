01d53ff2908ef3d1c753b84e2789cb6dddc7b793
Szymon Janota <szymon.janota@relayr.io>
2017-11-09 23:07:06 +0100

    sjanota: calculations explained

--- src/main/kotlin/com/github/sursmobil/werner/calc/StageCalculator.kt
+++ src/main/kotlin/com/github/sursmobil/werner/calc/StageCalculator.kt
@@ -26,10 +26,13 @@ class StageCalculator(val stage: Stage) {
         val totalMass = stage.rawMass + fuelMass
         val thrustForce = thrustForce(env)
 
+        // dV = integrate thrust / (totalMass - t * fuelMassUsage) for t=0 to burnTime
         return -thrustForce * Math.log(1 - burnTime * fuelMassUsage / totalMass) / fuelMassUsage
     }
 
     private fun requiredFuel(maneuver: Maneuver): Double {
+        // deltaVForFuelVol(Vf) = maneuver.dV , find Vf
+
         val c1 = - maneuver.dV * stage.engine.fuelMassUsage / thrust(maneuver)
         val c2 = 1 - Math.exp(c1)
         val c3 = stage.rawMass * c2
