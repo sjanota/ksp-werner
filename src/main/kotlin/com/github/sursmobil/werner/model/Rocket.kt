c20b77c08d2f032148cfb72cf8447de7da798b22
Szymon Janota <szymon.janota@relayr.io>
2017-11-16 20:36:47 +0100

    sjanota: Rocket refactor

--- src/main/kotlin/com/github/sursmobil/werner/model/Rocket.kt
+++ src/main/kotlin/com/github/sursmobil/werner/model/Rocket.kt
@@ -2,13 +2,17 @@ package com.github.sursmobil.werner.model
 
 import com.github.sursmobil.werner.StageCalculator
 
-class Rocket(private val stack: List<StageCalculator> = listOf(StageCalculator(Stage.Empty))) {
-    fun nextStages(payload: Payload, f: (StageCalculator) -> List<StageCalculator>): List<Rocket> {
-        val nextStage = Stage(Engine.None, stack.last().stage + payload)
-        val next = f(StageCalculator(nextStage, stack.last().restrictions))
-        return next.map { Rocket(stack + listOf(it)) }
-    }
-
+class Rocket(
+        private val stack: List<StageCalculator> = listOf(StageCalculator(Stage.Empty))
+) {
     val cost: Int = stack.last().stage.cost
     val stages = stack.map { it.stage }
+
+    fun nextStages(payload: Payload, f: (StageCalculator) -> List<StageCalculator>): List<Rocket> {
+        val newPayload = stack.last().stage + payload
+        val nextStage = Stage(engine = Engine.None, payload = newPayload)
+        val nextStageCalc = StageCalculator(nextStage, stack.last().restrictions)
+        val nextStages = f(nextStageCalc)
+        return nextStages.map { Rocket(stack + listOf(it)) }
+    }
 }
